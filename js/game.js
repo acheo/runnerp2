b4006de51941c6724060962daf70db17c328807f
acheo <adam@cheong.co>
2014-08-17 10:34:56 +0100

    refactor and tidy

--- js/game.js
+++ js/game.js
@@ -1,83 +1,83 @@
-	var game;
-	var ball;
-	var runner;
-	var jumping = false;
-	var kicking = false;
-	var started = false;
-	var text1;
-	var progress_empty;
-	var progress_full;
-	var pb; // platformbuilder
-	var CG_Terrain;
-	var CG_Ball;
-	var CG_Runner;
-	var bg;
+    var game;
+    var ball;
+    var runner;
+    var jumping = false;
+    var kicking = false;
+    var started = false;
+    var text1;
+    var progress_empty;
+    var progress_full;
+    var pb; // platformbuilder
+    var CG_Terrain;
+    var CG_Ball;
+    var CG_Runner;
+    var bg;
     var gameConfig;
-	
-	var lastRunnerX = 0;
+    
+    var lastRunnerX = 0;
 
     window.onload = function() {
-	
-		game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update, render: render });
-		
-		pb = new PGE.PlatformBuilder(game);
-		
+    
+        game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update, render: render });
+        
+        pb = new PGE.PlatformBuilder(game);
+        
         function preload () {
 
-			// load progress bar art
-			game.load.image('progressbar_empty', 'assets/ui/progress_empty.png');
-			game.load.image('progressbar', 'assets/ui/progress.png');
-		
-			game.stage.backgroundColor = '#50b1d4';
-			
+            // load progress bar art
+            game.load.image('progressbar_empty', 'assets/ui/progress_empty.png');
+            game.load.image('progressbar', 'assets/ui/progress.png');
+        
+            game.stage.backgroundColor = '#50b1d4';
+            
         }
 
         function create () {
-			
-			// create temporary progress bar sprites
-			progress_empty = game.add.sprite((800-600)/2,(600-40)/2, 'progressbar_empty');
-			progress_full = game.add.sprite((800-600)/2,(600-40)/2, 'progressbar');
-			game.load.setPreloadSprite(progress_full);
-		
-			 //	load status
-			text1 = game.add.text(280, (600-40)/2, '', { fill: '#FFFFFF'});
-			game.load.onFileComplete.add(fileComplete, this);
-			game.load.onLoadComplete.add(loadComplete, this);
-			
-			// queue assets for loading
-			game.load.image('bg', 'assets/background1.jpg');
-			game.load.image('tile1', 'assets/sprites/terrain/tile1.png');
-			game.load.image('tile2', 'assets/sprites/terrain/tile2.png');
-			game.load.image('tile3', 'assets/sprites/terrain/tile3.png');
-			game.load.image('tile4', 'assets/sprites/terrain/tile4.png');
-			game.load.image('tile5', 'assets/sprites/terrain/tile5.png');
-			game.load.image('tile6', 'assets/sprites/terrain/tile6.png');
-			game.load.image('tile7', 'assets/sprites/terrain/tile7.png');
-			
-			game.load.image('ball', 'assets/sprites/soccer.png');
-			game.load.physics('tilepolygons', 'assets/sprites/terrain/tiles.json');
-			game.load.atlasJSONHash('run', 'assets/sprites/running.png', 'assets/sprites/running.json');
-			game.load.atlasJSONHash('jump', 'assets/sprites/jumping.png', 'assets/sprites/jumping.json');
-			game.load.atlasJSONHash('kick', 'assets/sprites/kicking.png', 'assets/sprites/kicking.json');
-			
+            
+            // create temporary progress bar sprites
+            progress_empty = game.add.sprite((800-600)/2,(600-40)/2, 'progressbar_empty');
+            progress_full = game.add.sprite((800-600)/2,(600-40)/2, 'progressbar');
+            game.load.setPreloadSprite(progress_full);
+        
+             // load status
+            text1 = game.add.text(280, (600-40)/2, '', { fill: '#FFFFFF'});
+            game.load.onFileComplete.add(fileComplete, this);
+            game.load.onLoadComplete.add(loadComplete, this);
+            
+            // queue assets for loading
+            game.load.image('bg', 'assets/background1.jpg');
+            game.load.image('tile1', 'assets/sprites/terrain/tile1.png');
+            game.load.image('tile2', 'assets/sprites/terrain/tile2.png');
+            game.load.image('tile3', 'assets/sprites/terrain/tile3.png');
+            game.load.image('tile4', 'assets/sprites/terrain/tile4.png');
+            game.load.image('tile5', 'assets/sprites/terrain/tile5.png');
+            game.load.image('tile6', 'assets/sprites/terrain/tile6.png');
+            game.load.image('tile7', 'assets/sprites/terrain/tile7.png');
+            
+            game.load.image('ball', 'assets/sprites/soccer.png');
+            game.load.physics('tilepolygons', 'assets/sprites/terrain/tiles.json');
+            game.load.atlasJSONHash('run', 'assets/sprites/running.png', 'assets/sprites/running.json');
+            game.load.atlasJSONHash('jump', 'assets/sprites/jumping.png', 'assets/sprites/jumping.json');
+            game.load.atlasJSONHash('kick', 'assets/sprites/kicking.png', 'assets/sprites/kicking.json');
+            
             game.load.text('gameConfig', 'assets/gameConfig.json');
-			// start loading
-			game.load.start();
-		
+            // start loading
+            game.load.start();
+        
         }
 
-//	This callback is sent the following parameters:
+//  This callback is sent the following parameters:
 function fileComplete(progress, cacheKey, success, totalLoaded, totalFiles) {
 
-	text1.setText("Loaded: " + progress + "% - " + totalLoaded + " / " + totalFiles);
+    text1.setText("Loaded: " + progress + "% - " + totalLoaded + " / " + totalFiles);
 
 }
 
 function loadComplete() {
 
-	game.world.remove(text1);
-	game.world.remove(progress_empty);
-	start();
+    game.world.remove(text1);
+    game.world.remove(progress_empty);
+    start();
 
 }
     
@@ -85,218 +85,159 @@ function loadComplete() {
 function start() {
             var a = game.cache.getText('gameConfig');
             gameConfig = JSON.parse(a);
-			// add background image
-			bg = game.add.image(0, 0, 'bg');
-			bg.scale.set(0.6);
+            // add background image
+            bg = game.add.image(0, 0, 'bg');
+            bg.scale.set(0.6);
 
-			// creation of large world bounds
+            // creation of large world bounds
             game.world.bounds = new Phaser.Rectangle(-800, -600, 800*3, 600*3);  
-					
-			//	Enable p2 physics with gravity
-			game.physics.startSystem(Phaser.Physics.P2JS);
-			game.physics.p2.gravity.y = gameConfig.gravity;
-			
-			game.physics.p2.friction = 1;
-            game.physics.p2.restitution = 0.1;
-			
-			// add some tiles to platformbuilder
-			
-			pb.addTile('tile1');
-			pb.addTile('tile1');
-			pb.addTile('tile2');
-			pb.addTile('tile5');
-			pb.addTile('tile1');
-			pb.addTile('tile1');
-			pb.addTile('tile1');
-			pb.addTile('tile3');
-			pb.addTile('tile4');
-			pb.addTile('tile1');
-			pb.addTile('tile1');
-			pb.addTile('tile6');
-			pb.addTile('tile7');
-			
-			for (var i=1;i<20;i++){
-				pb.addTile('tile1');
-			}
-			
-			pb.addTile('tile3');
-			pb.addTile('tile4',72*4); // with a 4x 72px gap before it
-			pb.addTile('tile1');
-			pb.addTile('tile1');
-			pb.addTile('tile6');
-			pb.addTile('tile7',72*3);
-			
-			for (var i=1;i<20;i++){
-				pb.addTile('tile1');
-			}
-			
-			pb.addTile('tile2');
-			pb.addTile('tile5');
-			pb.addTile('tile1');
-			pb.addTile('tile1');
-			pb.addTile('tile1');
-			pb.addTile('tile3');
-			pb.addTile('tile7');
-			pb.addTile('tile1');
-			pb.addTile('tile1');
-			pb.addTile('tile6');
-			pb.addTile('tile4');
-			
-			for (var i=1;i<60;i++){
-				pb.addTile('tile1');
-			}
-			
-			CG_Terrain = game.physics.p2.createCollisionGroup();
-			CG_Ball = game.physics.p2.createCollisionGroup();
-			CG_Runner = game.physics.p2.createCollisionGroup();
-			//game.physics.p2.updateBoundsCollisionGroup();
-			
-			// create ball sprite
-			ball = game.add.sprite(500, 50, 'ball');
-			ball.scale.set(0.1);
-	
-			//  runner sprite is using a texture atlas for all of its animation data
-			runner = game.add.sprite(50, 350, 'runner');	
-			runner.scale.set(0.8);
-			runner.loadTexture('run', 0);
-			runner.animations.add('run');
-			runner.animations.play('run', 30, true);
-			
-			// enable physics on non platform sprites
-			game.physics.p2.enable([ball,runner]);
-			//game.physics.p2.enable([ball]);
-			//game.physics.p2.enable([runner], true);
+                    
+            //  Enable p2 physics with gravity
+            game.physics.startSystem(Phaser.Physics.P2JS);
+            game.physics.p2.gravity.y = gameConfig.gravity;
+            
+            game.physics.p2.friction = gameConfig.friction;
+            game.physics.p2.restitution = gameConfig.restitution;
+            
+            // create terrain from tile id sequence in game config
+            pb.addTiles(gameConfig.terrain);
+            
+            // Create separate collision groups for terrain, runner and ball to allow independent collisions (terrain-ball and terrain-runner)
+            CG_Terrain = game.physics.p2.createCollisionGroup();
+            CG_Ball = game.physics.p2.createCollisionGroup();
+            CG_Runner = game.physics.p2.createCollisionGroup();
+            
+            // create ball sprite
+            ball = game.add.sprite(500, 50, 'ball');
+            ball.scale.set(0.1);
+    
+            //  runner sprite is using a texture atlas for all of its animation data
+            runner = game.add.sprite(50, 350, 'runner');    
+            runner.scale.set(0.8);
+            runner.loadTexture('run', 0);
+            runner.animations.add('run');
+            runner.animations.play('run', 30, true);
+                        
+            // enable physics on non platform sprites
+            if (gameConfig.debug) {
+                game.physics.p2.enable([ball]);
+                game.physics.p2.enable([runner], true);
+            } else {
+                game.physics.p2.enable([ball,runner]);
+            }
+            
+            // enable physics on platform sprites
+            pb.enablePhysics();
+            
+            // load collision polygons for platform sprites
+            pb.loadPolygons(CG_Terrain,[CG_Ball,CG_Runner]);
+        
+            // add circle body to ball
+            ball.body.clearShapes();
+            ball.body.addCircle(ball.width*0.5);
+            ball.body.setCollisionGroup(CG_Ball);
+            ball.body.collides(CG_Terrain);
+            
+            runner.body.setCollisionGroup(CG_Runner);
+            runner.body.collides(CG_Terrain);
+            
+            runner.body.data.gravityScale = 2;
+            
+            game.camera.bounds = null; // disables camera bounds constraints
+            game.camera.x = 0;
+            
+            started = true;
+        
+        }
+        
+        function update() {
 
-			// enable physics on platform sprites
-			pb.enablePhysics();
-			
-			// load collision polygons for platform sprites
-			pb.loadPolygons(CG_Terrain,[CG_Ball,CG_Runner]);
-		
-			// add circle body to ball
-			ball.body.clearShapes();
-			ball.body.addCircle(ball.width*0.5);
-			ball.body.setCollisionGroup(CG_Ball);
-			ball.body.collides(CG_Terrain);
-			
-			runner.body.setCollisionGroup(CG_Runner);
-			runner.body.collides(CG_Terrain);
-			
-			runner.body.data.gravityScale = 2;
-			
-		
-			/*
-			game.camera.bounds = game.world.bounds;
-			game.camera.bounds.x = 0;
-			game.camera.bounds.y = 0;
-			game.camera.bounds.height = 600;
-			*/
-			
-			
-			game.camera.bounds = null;
-			game.camera.x = 0;
-			
-			
-			//game.camera.follow(runner, Phaser.Camera.FOLLOW_PLATFORMER);
-			//game.camera.follow(runner);
-			
-			started = true;
-		
-		}
-		
-		function update() {
-
-			if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-			
-				if (!jumping && !kicking){
-					jumping = true;
-					runner.loadTexture('jump', 0);
-					var anim = runner.animations.add('jump');					
-					anim.onComplete.add(jumpCompleted, this);
-					runner.animations.play('jump', gameConfig.jump.frameRate);
-					runner.body.velocity.y += gameConfig.jump.runner_vx_change;
-				}
-						
-			}
-			
-			
-			if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+            if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+            
+                if (!jumping && !kicking){
+                    jumping = true;
+                    runner.loadTexture('jump', 0);
+                    var anim = runner.animations.add('jump');                   
+                    anim.onComplete.add(jumpCompleted, this);
+                    runner.animations.play('jump', gameConfig.jump.frameRate);
+                    runner.body.velocity.y += gameConfig.jump.runner_vx_change;
+                }
+                        
+            }
+            
+            
+            if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
                 var dist = runner.x + runner.width - ball.x;
                 console.log(dist);
-			     var canReachBall = Math.abs(dist) < gameConfig.kick.ball_runner_maxDistance;
-				if (!kicking && !jumping && canReachBall){
-					kicking = true;
-		
-					runner.loadTexture('kick', 0);
-					var anim = runner.animations.add('kick');		
-					anim.onComplete.add(kickCompleted, this);
-					runner.animations.play('kick', gameConfig.kick.frameRate);
-					// todo: runner to ball range check...					
-					ball.body.velocity.x += gameConfig.kick.ball_vx_change;
-					ball.body.velocity.y += gameConfig.kick.ball_vy_change;
-					//ball.body.mass = 100;
-					
-				}
-			
-			
-			}
-			
-			if (started){
-				//game.camera.x+=5;
-				runner.body.x = lastRunnerX + 10;
-				
-				lastRunnerX = runner.body.x;
-				
-				//runner.body.data.force[0] = 0;
-				//runner.body.data.force[1] = -50;
-				
-				game.camera.x = runner.body.x - 100;
-				
-				if (game.camera.x < 0) game.camera.x = 0;
-				
-				if (runner.body.angle > 30) runner.body.angle = 30;
-				if (runner.body.angle < -30) runner.body.angle = -30;
-				
-				if (ball.body.x < runner.body.x+runner.width/2) ball.body.x = runner.body.x+runner.width/2;
-				
-				// p-lax
-				
-				bg.x = game.camera.x/2;
-				
-			}
-			
-			
-		
-		}
-		
-		function jumpCompleted(){
-		
-			jumping=false;
-			kicking=false;
-			
-			runner.loadTexture('run', 0);
-			var anim = runner.animations.add('run');
-			runner.animations.play('run', 30,true);
-		
-		}
-	
-		function kickCompleted(){
-		
-			jumping=false;
-			kicking=false;
-			
-			runner.loadTexture('run', 0);
-			var anim = runner.animations.add('run');
-			runner.animations.play('run', 30,true);
-		
-		}
-		
-		function render() {
-			//game.debug.cameraInfo(game.camera, 32, 32);
-			
-			if (runner != null) {
-				//game.debug.spriteCoords(runner, 32, 500);
-			}
+                 var canReachBall = Math.abs(dist) < gameConfig.kick.ball_runner_maxDistance;
+                if (!kicking && !jumping && canReachBall){
+                    kicking = true;
+        
+                    runner.loadTexture('kick', 0);
+                    var anim = runner.animations.add('kick');       
+                    anim.onComplete.add(kickCompleted, this);
+                    runner.animations.play('kick', gameConfig.kick.frameRate);
+                    
+                    // todo: runner to ball range check...                  
+                    ball.body.velocity.x += gameConfig.kick.ball_vx_change;
+                    ball.body.velocity.y += gameConfig.kick.ball_vy_change;
+                    
+                }
+            
+            
+            }
+            
+            if (started){
 
-		}
+                runner.body.x = lastRunnerX + 10;               
+                lastRunnerX = runner.body.x;                
+                game.camera.x = runner.body.x - 100;
+                
+                if (game.camera.x < 0) game.camera.x = 0;
+                
+                if (runner.body.angle > 30) runner.body.angle = 30;
+                if (runner.body.angle < -30) runner.body.angle = -30;
+                
+                if (ball.body.x < runner.body.x+runner.width/2) ball.body.x = runner.body.x+runner.width/2;
+                
+                // parallax                
+                bg.x = game.camera.x/2;
+                
+            }
+            
+            
+        
+        }
+        
+        function jumpCompleted(){
+        
+            jumping=false;
+            kicking=false;
+            
+            runner.loadTexture('run', 0);
+            var anim = runner.animations.add('run');
+            runner.animations.play('run', 30,true);
+        
+        }
+    
+        function kickCompleted(){
+        
+            jumping=false;
+            kicking=false;
+            
+            runner.loadTexture('run', 0);
+            var anim = runner.animations.add('run');
+            runner.animations.play('run', 30,true);
+        
+        }
+        
+        function render() {
+            
+            if (started) {
+                if (gameConfig.debug){
+                    game.debug.spriteCoords(runner, 32, 500);
+                }
+            }
+
+        }
     };
--- js/platformbuilder.js
+++ js/platformbuilder.js
@@ -2,72 +2,93 @@ var PGE = PGE || {}; // Namespace - Platform Game Engine
 
 PGE.PlatformBuilder = function (game) {
 
-	this._game = game;
-	this.tiles = [];
-	this.nextTileX = 0;
-	
+    this._game = game;
+    this.tiles = [];
+    this.nextTileX = 0;
+    
 };
 
 PGE.PlatformBuilder.prototype = {
 
-	addTile: function(imgName,gapSize){
-			
-		var newTile = {
-		
-			imgName: imgName,
-			sprite: this._game.add.sprite(0,0,imgName)
-		
-		}
-		newTile.sprite.scale.set(0.5);
-		
-		newTile.sprite.x = this.nextTileX+newTile.sprite.width*0.5; 
-		newTile.sprite.y = 600 - newTile.sprite.height*0.5;
-		
-		this.nextTileX+=newTile.sprite.width;
-		
-		if (gapSize != null){
-			newTile.sprite.x += gapSize;
-			this.nextTileX+= gapSize;
-		}
-		
-		this.tiles.push(newTile);
-	
-	},
-	
-	clear: function(){
-	
-		this.tiles = [];
-		this.nextTileX = 0;
-	
-	},
-	
-	enablePhysics: function(){
-	
-		var tileSprites = [];
-		for (var t=0;t<this.tiles.length;t++){
-			var tile = this.tiles[t];
-			tileSprites.push(tile.sprite);
-		}
-		
-		this._game.physics.p2.enable(tileSprites);
-		
-	
-	},
-	
-	loadPolygons: function(cg,collideslist){
-	
-		for (var t=0;t<this.tiles.length;t++){
-			var tile = this.tiles[t];
-			// load polys apart from tile1 = rectangle
-			if (tile.imgName != 'tile1') {
-				tile.sprite.body.clearShapes();
-				tile.sprite.body.loadPolygon('tilepolygons', tile.imgName,0.5);
-			}
-			tile.sprite.body.static=true;
-			tile.sprite.body.setCollisionGroup(cg);
-			tile.sprite.body.collides(collideslist);
-		}
-	
-	}
+    // add a single tile
+    addTile: function(imgName,gapSize){
+            
+        var newTile = {
+        
+            imgName: imgName,
+            sprite: this._game.add.sprite(0,0,imgName)
+        
+        }
+        newTile.sprite.scale.set(0.5);
+        
+        newTile.sprite.x = this.nextTileX+newTile.sprite.width*0.5; 
+        newTile.sprite.y = 600 - newTile.sprite.height*0.5;
+        
+        this.nextTileX+=newTile.sprite.width;
+        
+        if (gapSize != null){
+            newTile.sprite.x += gapSize;
+            this.nextTileX+= gapSize;
+        }
+        
+        this.tiles.push(newTile);
+    
+    },
+    
+    // add many tiles by id number array
+    addTiles: function(id_array){
+    
+        for (var i=0;i<id_array.length;i++){
+        
+            var tileId = id_array[i];
+            
+            if (tileId == 0) {
+                // empty tile ? (72px gap)
+                this.nextTileX+= 72;
+            } else {
+                // regular tile
+                var imgName = 'tile'+tileId;
+                this.addTile(imgName,null);
+            }
+        
+        }
+    
+    },
+    
+    clear: function(){
+    
+        this.tiles = [];
+        this.nextTileX = 0;
+    
+    },
+    
+    enablePhysics: function(){
+    
+        var tileSprites = [];
+        for (var t=0;t<this.tiles.length;t++){
+            var tile = this.tiles[t];
+            tileSprites.push(tile.sprite);
+        }
+        
+        this._game.physics.p2.enable(tileSprites);
+        
+    
+    },
+    
+    loadPolygons: function(cg,collideslist){
+    
+        for (var t=0;t<this.tiles.length;t++){
+            var tile = this.tiles[t];
+            // load polys apart from tile1 = rectangle
+            if (tile.imgName != 'tile1') {
+                tile.sprite.body.clearShapes();
+                tile.sprite.body.loadPolygon('tilepolygons', tile.imgName,0.5);
+            }
+            tile.sprite.body.static=true;
+            tile.sprite.body.setCollisionGroup(cg);
+            tile.sprite.body.collides(collideslist);
+        }
+    
+    }
 
 };
